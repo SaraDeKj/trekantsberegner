9adac9de0d7ea155e70b5f20054502c0bbdd000a
sarad <saradekjae@gmail.com>
2024-03-07 09:56:48 +0100

    Første kode

---
+++ main.py
@@ -0,0 +1,71 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+print("velkommen til trekantsbergneren")
+valg = (input("vælg kendt information A, B, C, a, b eller c"))
+print(valg,"er", defineringafvariabel)
+valg2 =(input("vælg kendt information A, B, C, a, b eller c"))
+print(valg2,"er", defineringafvariabel))
+valg3 =(input("vælg kendt information A, B, C, a, b eller c"))
+print(valg3,"er", defineringafvariabel)
+
+if "a" and "c" and ("A" or "B")==True:
+    print(sinusrelation)
+
+if "A"and "B" and "C" ==True:
+    print("fejl")
+    exit()
+
+elif "a" and "b" and "c" == True:
+    print ("cosinusrelation function")
+
+def defineringafvariabel():
+    if "A" in valg:
+        A = int(input("indtast værdi af A"))
+        if A < 0 or A > 360:
+            print("fejl")
+            exit()
+        else:
+            print("A er", A)
+
+    elif "B" in valg:
+        B = (int("indtast værdi af B"))
+        if B < 0 or B > 360:
+            print("fejl")
+            exit()
+        else:
+            print("B er", B)
+
+    elif "C" in valg:
+        C = (int("indtast værdi af B"))
+        if C < 0 or C > 360:
+            print("fejl")
+            exit()
+        else:
+            print("C er", C)
+
+    elif "a" in valg:
+        a = (int("indtast værdi af B"))
+        if a < 0 or a > 360:
+            print("fejl")
+            exit()
+        else:
+            print("a er", a)
+    elif "b" in valg:
+        b = (int("indtast værdi af B"))
+        if b < 0 or b > 360:
+            print("fejl")
+            exit()
+        else:
+            print("b er", b)
+
+
+
+
+
+
+
+
+
