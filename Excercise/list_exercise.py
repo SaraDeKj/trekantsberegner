4b49c0b51c9789bb274f3b786d03453eef1a91a6
sarad <saradekjae@gmail.com>
2024-03-14 10:16:29 +0100

    funktion der summer listen

--- Excercise/list_exercise.py
+++ Excercise/list_exercise.py
@@ -1,9 +1,12 @@
 crazyliste=[1,2,3,4,5,6,7,8,9,10]
 print(len(crazyliste))
+
 crazyliste.append(11)
 print(crazyliste)
+
 crazyliste.insert(0,0)
 print(crazyliste )
+
 crazyliste.remove(len(crazyliste )-1)
 print(crazyliste )
 
@@ -19,5 +22,15 @@ crazyliste.sort()
 print(crazyliste )
 
 crazyliste= [x**2 for x in crazyliste]
-
 print(crazyliste)
+
+#Funktion der giver sum af alle elemnter i listen
+def summning(liste):
+    sum=0
+    for element in liste:
+        sum += element
+        x= element
+    return sum
+
+res=summning(crazyliste )
+print("summen af listen =",res)
